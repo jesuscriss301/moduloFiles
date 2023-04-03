0ac1d904ddf269e06ad6d7cf2f1120b75237c4bf
Carboexco2 <124207086+Carboexco2@users.noreply.github.com>
2023-04-03 10:23:00 -0500

    visualizar pdf

--- src/main/java/com/carboexco/moduloFiles/controller/FileController.java
+++ src/main/java/com/carboexco/moduloFiles/controller/FileController.java
@@ -1,21 +1,24 @@
 package com.carboexco.moduloFiles.controller;
 
+import com.carboexco.moduloFiles.entity.File;
+import com.carboexco.moduloFiles.entity.FileService;
+import com.carboexco.moduloFiles.repository.FileRepository;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.util.StringUtils;
-import com.carboexco.moduloFiles.repository.*;
-import com.carboexco.moduloFiles.entity.*;
 
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Optional;
 
 @RestController
@@ -91,6 +94,26 @@ public class FileController {
             ResponseEntity<Resource> response = new ResponseEntity<Resource>(
                     file, headers, HttpStatus.OK);
             return response;
+        }
+        return null;
+    }
+
+    @GetMapping("/filesPDF/view/{id}")
+    public ResponseEntity<InputStreamResource> getPDF(@PathVariable int id) throws IOException {
+        Optional<File> idfotografia = idfotografiaRepository.findById(id);
+
+        if (idfotografia.isPresent()) {
+            File data = idfotografia.get();
+            Resource file = fileService.loadFile(data.getId() + data.getNombreArchivo(), data.getDireccionCarpeta());
+            InputStream inputStream = new FileInputStream("C:/img/"+data.getDireccionCarpeta()+"/"+data.getId() + data.getNombreArchivo());
+            InputStreamResource inputStreamResource = new InputStreamResource(inputStream);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", "inline; filename="+data.getId() + data.getNombreArchivo());
+
+            return ResponseEntity.ok()
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_PDF)
+                .body(inputStreamResource);
         }
         return null;
     }
